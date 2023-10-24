922d45cc034248a38aa75e5b2145c9dac993e6fc
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2023-10-24 16:26:38 +0700

    Organize two main apps

--- app.py
+++ app.py
@@ -101,81 +101,87 @@ source_values = ['None'] + list(df['source'].unique())
 # health_values = ['None'] + list(df['health'].unique())
 
 #########################################
-## Select options
-## TODO: family, costume
+## Select Main Program
 
-# with st.sidebar:
-#     st.radio
-col1, col2, col3 = st.columns(3)
+with st.sidebar:
+    genre = st.radio(
+    "Choose how to explore heroes",
+    [":rainbow[Heroes Explorer]", "***LB/CB Hero Stat*** :movie_camera:"],
+    captions = ["Filter only heroes with certain properties", "Co-powered by Elioty33's DataVault"])
 
-with col1:
-    st.header("Standard Filters:")
-    st.write("Tips: filter costume by typing ' C' or 'C2' in the Name box")
-    with st.expander("Filter Options"):
-        name_option = st.text_input(label="Name:", value="")
-        star_option = st.selectbox(label='Star:', options=star_values, index=0)
-        color_option = st.selectbox(label='Color:', options=color_values, index=0)
-        speed_option = st.selectbox(label='Speed:', options=speed_values, index=0)
-        class_option = st.selectbox(label='Class:', options=class_values, index=0)
-        source_option = st.selectbox(label='Origin:', options=source_values, index=0)
-    
-        special_type_option = st.text_input(label="SpecialSkill Category", value="Hit 3")
-        special_text_option = st.text_input(label="SpecialSkill Text", value="Dispel")
-
-
-with col2:
-    st.header("Stat Filters")
-    with st.expander("Stat Options"):   
-        defense_option = st.text_input(label="Defense:", value="0")
-        attack_option = st.text_input(label="Attack:", value="0")
-        health_option = st.text_input(label="Health:", value="0")
-
-with col3:
-    st.header("Sorted By (or directly click at the column name)")
-    sort_option = st.selectbox(label='Sort by', options=display_cols[1:], index=5) # default is power
-    
-idx_all = []
-
-if name_option != '':
-    idx_all.append(filter_by_1col(df, 'name', name_option, exact_flag=False)) 
-
-if star_option != 'None':
-    idx_all.append(filter_by_1col_num(df, 'star', star_option, oper_flag="eq"))    
-
-if speed_option != 'None':
-    idx_all.append(filter_by_1col(df, 'speed', speed_option, exact_flag=True))    
-
-if color_option != 'None':
-    idx_all.append(filter_by_1col(df, 'color', color_option, exact_flag=False))    
-
-if class_option != 'None':
-    idx_all.append(filter_by_1col(df, 'class', class_option, exact_flag=False))    
-
-if source_option != 'None':
-    idx_all.append(filter_by_1col(df, 'source', source_option, exact_flag=False))    
-
-if defense_option != "0":
-    defense_option = int(defense_option)
-    idx_all.append(filter_by_1col_num(df, 'defense', defense_option, oper_flag="ge"))   
-
-if attack_option != "0":
-    attack_option = int(attack_option)
-    idx_all.append(filter_by_1col_num(df, 'attack', attack_option, oper_flag="ge"))   
-
-if health_option != "0":
-    health_option = int(health_option)
-    idx_all.append(filter_by_1col_num(df, 'health', health_option, oper_flag="ge"))   
-
-
-if special_type_option  != '':
-    idx_all.append(filter_by_1col(df, 'types', special_type_option, exact_flag=False))    
-
-if special_text_option != '':
-    idx_all.append(filter_by_1col(df, 'effects', special_text_option, exact_flag=False))    
-    
 #########################################
-st.header(f'Updated: Oct 23, 2023 -- Total heroes = {len(df)}')
+## Program 1
+if genre == ':rainbow[Heroes Explorer]':
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Standard Filters:")
+        st.write("Tips: filter costume by typing ' C' or 'C2' in the Name box")
+        with st.expander("Filter Options"):
+            name_option = st.text_input(label="Name:", value="")
+            star_option = st.selectbox(label='Star:', options=star_values, index=0)
+            color_option = st.selectbox(label='Color:', options=color_values, index=0)
+            speed_option = st.selectbox(label='Speed:', options=speed_values, index=0)
+            class_option = st.selectbox(label='Class:', options=class_values, index=0)
+            source_option = st.selectbox(label='Origin:', options=source_values, index=0)
+    
+            special_type_option = st.text_input(label="SpecialSkill Category", value="Hit 3")
+            special_text_option = st.text_input(label="SpecialSkill Text", value="Dispel")
+    with col2:
+        st.header("Stat Filters")
+        with st.expander("Stat Options"):   
+            defense_option = st.text_input(label="Defense:", value="0")
+            attack_option = st.text_input(label="Attack:", value="0")
+            health_option = st.text_input(label="Health:", value="0")
+    with col3:
+        st.header("Sorted By (or directly click at the column name)")
+        sort_option = st.selectbox(label='Sort by', options=display_cols[1:], index=5) # default is power
+    
+    idx_all = []
 
-df2 = df[np.all(idx_all,axis=0)]
+    if name_option != '':
+        idx_all.append(filter_by_1col(df, 'name', name_option, exact_flag=False)) 
 
-display_heroes_from_df(df2.sort_values(sort_option, ascending=False))
+    if star_option != 'None':
+        idx_all.append(filter_by_1col_num(df, 'star', star_option, oper_flag="eq"))    
+
+    if speed_option != 'None':
+        idx_all.append(filter_by_1col(df, 'speed', speed_option, exact_flag=True))    
+
+    if color_option != 'None':
+        idx_all.append(filter_by_1col(df, 'color', color_option, exact_flag=False))    
+
+    if class_option != 'None':
+        idx_all.append(filter_by_1col(df, 'class', class_option, exact_flag=False))    
+
+    if source_option != 'None':
+        idx_all.append(filter_by_1col(df, 'source', source_option, exact_flag=False))    
+
+    if defense_option != "0":
+        defense_option = int(defense_option)
+        idx_all.append(filter_by_1col_num(df, 'defense', defense_option, oper_flag="ge"))   
+
+    if attack_option != "0":
+        attack_option = int(attack_option)
+        idx_all.append(filter_by_1col_num(df, 'attack', attack_option, oper_flag="ge"))   
+
+    if health_option != "0":
+        health_option = int(health_option)
+        idx_all.append(filter_by_1col_num(df, 'health', health_option, oper_flag="ge"))   
+
+    if special_type_option  != '':
+        idx_all.append(filter_by_1col(df, 'types', special_type_option, exact_flag=False))    
+
+    if special_text_option != '':
+        idx_all.append(filter_by_1col(df, 'effects', special_text_option, exact_flag=False))    
+    
+    #########################################
+    st.header(f'Updated: Oct 23, 2023 -- Total heroes = {len(df)}')
+
+    df2 = df[np.all(idx_all,axis=0)]
+
+    display_heroes_from_df(df2.sort_values(sort_option, ascending=False))
+#########################################
+## Program 2
+else:
+    st.write(" Work in progress")
